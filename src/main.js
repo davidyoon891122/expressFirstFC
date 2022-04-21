46a80e502797ca761c504c067b8f0186702f4166
davidyoon <qqpo12@naver.com>
2022-04-21 21:23:35 +0900

    add nodemon add middleware sequence with next()

--- src/main.js
+++ src/main.js
@@ -7,7 +7,24 @@ const app = express()
 
 const PORT = 5005
 
-app.use('/', (req, res) => {
+app.use(
+  '/',
+  (req, res, next) => {
+    console.log('Middleware 1-1')
+
+    setTimeout(() => {
+      next()
+    }, 1000)
+    // next() // 함수가 끝났으니 다음 것을 호출하라는 의미
+  },
+  (req, res, next) => {
+    console.log('Middleware 1-2')
+    next()
+  }
+)
+
+app.use((req, res) => {
+  console.log('Middleware 2')
   res.send('Hello, Express!')
 })
 
