182f0ce1f597c9d27e790ba714eddd832a170518
davidyoon <qqpo12@naver.com>
2022-04-21 21:03:55 +0900

    create default webServer with express

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/* eslint-disable no-console */
+const express = require('express')
+
+const app = express()
+
+const PORT = 5005
+
+app.use('/', (req, res) => {
+  res.send('Hello, Express!')
+})
+
+app.listen(PORT, () => {
+  console.log(`The Express server is listening at port: ${PORT}`)
+})
