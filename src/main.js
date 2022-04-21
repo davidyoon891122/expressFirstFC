b19405e6f3401e00391de7a4e0a4560b5e783190
davidyoon <qqpo12@naver.com>
2022-04-21 21:34:38 +0900

    use async await

--- src/main.js
+++ src/main.js
@@ -2,22 +2,28 @@
 
 /* eslint-disable no-console */
 const express = require('express')
-
+const fs = require('fs')
 const app = express()
 
 const PORT = 5005
 
-app.use('/', (req, res, next) => {
+app.use('/', async (req, res, next) => {
   console.log('Middleware 1-1')
-  const requestedAt = new Date() // 다른 미들웨어로 requestedAt 정보를 넘기기
+  const fileContent = await fs.promises.readFile('.gitignore') // async await 활용
+
+  const requestedAt = new Date()
   // @ts-ignore
   req.requestedAt = requestedAt // 방법 1 req에 값을 넣어서 사용
+  // @ts-ignore
+  req.fileContent = fileContent
   next()
 })
 
 app.use((req, res) => {
   console.log('Middleware 2')
-  res.send(`Hello, Express!: Requested at ${req.requestedAt}`)
+  res.send(
+    `Hello, Express!: Requested at ${req.requestedAt}, ${req.fileContent}`
+  )
 })
 
 app.listen(PORT, () => {
