9b9cae04797bfa0f8324e00610cf47b14c6de208
davidyoon <qqpo12@naver.com>
2022-04-21 21:29:50 +0900

    throw value through middleWare first is using req

--- src/main.js
+++ src/main.js
@@ -7,25 +7,17 @@ const app = express()
 
 const PORT = 5005
 
-app.use(
-  '/',
-  (req, res, next) => {
-    console.log('Middleware 1-1')
-
-    setTimeout(() => {
-      next()
-    }, 1000)
-    // next() // 함수가 끝났으니 다음 것을 호출하라는 의미
-  },
-  (req, res, next) => {
-    console.log('Middleware 1-2')
-    next()
-  }
-)
+app.use('/', (req, res, next) => {
+  console.log('Middleware 1-1')
+  const requestedAt = new Date() // 다른 미들웨어로 requestedAt 정보를 넘기기
+  // @ts-ignore
+  req.requestedAt = requestedAt // 방법 1 req에 값을 넣어서 사용
+  next()
+})
 
 app.use((req, res) => {
   console.log('Middleware 2')
-  res.send('Hello, Express!')
+  res.send(`Hello, Express!: Requested at ${req.requestedAt}`)
 })
 
 app.listen(PORT, () => {
