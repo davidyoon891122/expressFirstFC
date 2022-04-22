eb1dde8aadd050d9726a4d7e6d4d301b2bdc992e
davidyoon <qqpo12@naver.com>
2022-04-23 00:23:47 +0900

    add pug for front end set default pug settings

--- src/main.js
+++ src/main.js
@@ -6,27 +6,39 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.set('views', 'src/views')
+app.set('view engine', 'pug')
 
 const PORT = 5005
 
 const userRouter = express.Router()
 
 const USERS = {
+  14: {
+    nickname: 'davidyoon',
+  },
   15: {
     nickname: 'foo',
   },
-  14: {
-    nickname: 'davidyoon',
+  16: {
+    nickname: 'bar',
   },
 }
 userRouter.get('/', (req, res) => {
   res.send('User list')
 })
 
+// /users/15
 userRouter.get('/:id', (req, res) => {
-  console.log('userRouter get ID')
-  // @ts-ignore
-  res.send(req.user) // express가 알아서 json.stringfy해줌 헤더에 applicaiton type도 json으로 자동 변경
+  const resMimeType = req.accepts(['json', 'html'])
+  if (resMimeType === 'json') {
+    // @ts-ignore
+    res.send(req.user)
+  } else if (resMimeType === 'html') {
+    res.render('user-profile', {
+      nickname: req.user.nickname,
+    })
+  }
 })
 
 userRouter.post('/', (req, res) => {
@@ -53,6 +65,12 @@ userRouter.param('id', (req, res, next, value) => {
 })
 
 app.use('/users', userRouter)
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    message: 'Hello, Pug!!',
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`The Express server is listening at port: ${PORT}`)
